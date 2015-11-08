431a63b5c49e2e096d85b876f92bb0e5ff111e5c
AvneetKhaira7 <akaur216@my.centennialcollege.ca>
2015-11-08 01:59:02 -0500

    Second commit. Using Approach 2.

--- src/Exercise1/Program.java
+++ src/Exercise1/Program.java
@@ -5,7 +5,7 @@ public class Program {
 	public static void main(String[] args) {
 		RollDie game = new RollDie(36000);
 		game.calculateResults();
-		game.displayResults();
+	
 
 	}
 
--- src/Exercise1/RollDie.java
+++ src/Exercise1/RollDie.java
@@ -1,6 +1,9 @@
 package Exercise1;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class RollDie {
 
@@ -10,17 +13,6 @@ public class RollDie {
 		private ArrayList<Integer> _sumOfDies = new ArrayList<Integer>();
 		private int _numTimesRolled;
 		private int _sum;
-		private int _count2 = 0;
-		private int _count3 = 0;
-		private int _count4 = 0;
-		private int _count5 = 0;
-		private int _count6 = 0;
-		private int _count7 = 0;
-		private int _count8 = 0;
-		private int _count9 = 0;
-		private int _count10 = 0;
-		private int _count11 = 0;
-		private int _count12 = 0;
 		
 		//CONSTRUCTOR TO INITIALIZE ++++++++++++++++++++++++++++++++++++++
 		public RollDie(int numTimesRolled){
@@ -47,57 +39,16 @@ public class RollDie {
 		
 		public void calculateResults(){
 			gameBegins();
-			for(int i=0; i<this._sumOfDies.size(); i++){
-				if(this._sumOfDies.get(i).equals(2)){
-					this._count2++;
-				}
-				else if(this._sumOfDies.get(i).equals(3)){
-					this._count3++;
-				}
-				else if(this._sumOfDies.get(i).equals(4)){
-					this._count4++;
-				}
-				else if(this._sumOfDies.get(i).equals(5)){
-					this._count5++;
-				}
-				else if(this._sumOfDies.get(i).equals(6)){
-					this._count6++;
-				}
-				else if(this._sumOfDies.get(i).equals(7)){
-					this._count7++;
-				}
-				else if(this._sumOfDies.get(i).equals(8)){
-					this._count8++;
-				}
-				else if(this._sumOfDies.get(i).equals(9)){
-					this._count9++;
-				}
-				else if(this._sumOfDies.get(i).equals(10)){
-					this._count10++;
-				}
-				else if(this._sumOfDies.get(i).equals(11)){
-					this._count11++;
-				}
-				else {
-					this._count12++;
-				}
+			Set<Integer> diceSet = new HashSet<Integer>(this._sumOfDies);
+			for(int temp: diceSet){
+				System.out.println("Number of times " + temp + " rolled: " + Collections.frequency(this._sumOfDies, temp));
+			
+			}
+			
 			}
 			
 		}
 		
-		public void displayResults(){
-			System.out.println("Number of times 2 rolled: " + this._count2);
-			System.out.println("Number of times 3 rolled: " + this._count3);
-			System.out.println("Number of times 4 rolled: " + this._count4);
-			System.out.println("Number of times 5 rolled: " + this._count5);
-			System.out.println("Number of times 6 rolled: " + this._count6);
-			System.out.println("Number of times 7 rolled: " + this._count7);
-			System.out.println("Number of times 8 rolled: " + this._count8);
-			System.out.println("Number of times 9 rolled: " + this._count9);
-			System.out.println("Number of times 10 rolled: " + this._count10);
-			System.out.println("Number of times 11 rolled: " + this._count11);
-			System.out.println("Number of times 12 rolled: " + this._count12);
-			//System.out.println("Hello");
-		}
 		
-}
+		
+
