3534126177078df217caab7389e993db769a333f
AvneetKhaira7 <akaur216@my.centennialcollege.ca>
2015-11-08 01:46:02 -0500

    Initial Commit. //Approach 1 for exercise 1.

---
+++ src/Exercise1/Program.java
@@ -0,0 +1,12 @@
+package Exercise1;
+
+public class Program {
+
+	public static void main(String[] args) {
+		RollDie game = new RollDie(36000);
+		game.calculateResults();
+		game.displayResults();
+
+	}
+
+}
